0971c94c5a4af6aa1f493f12daf7e6562ce23435
crgiraldo <cristian.giraldo@mercadolibre.com.co>
2025-02-04 12:12:13 -0500

    Express Server
    create a server in express
    
    added logs
    
    Create a server Express
    
    create a server in express
    
    testing cherry pick
    
    testing commits

---
+++ index.js
@@ -0,0 +1,25 @@
+//Create a express server
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Create a route
+
+app.get('/', (req, res) => {
+	console.log('Request received');
+	res.send('Hello World');
+
+	res.send('Hello World');
+	console.log('Hello Cris');
+	console.log('Hello Cris');
+	console.log('Hello Cris');
+	console.log('Hello Cris');
+	console.log('Hello Cris');
+}
+
+//Start the server
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+}
